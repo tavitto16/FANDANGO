39e2de43198ffead3cfc84c984f1ade111a421e7
Gustavo <ghp@gatv.ssr.upm.es>
2018-04-26 22:46:30 +0200

    subida inicial de las ontologias y el archivo de parseo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 FANDANGO
+
+## This repository has been created as part of the EC Funded FANDANGO Project developments	
---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 23 19:54:07 2018
+
+@author: gustavo
+"""
+
+import json
+import pronto
